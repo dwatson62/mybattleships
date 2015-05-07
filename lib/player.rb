098c3c0627a6c5bb0c6d7f51c1d804eeb0d5307c
Daryl Watson <darylwtsn@gmail.com>
2015-05-07 16:10:28 +0100

    Can now hit ships and see it

--- lib/player.rb
+++ lib/player.rb
@@ -29,18 +29,28 @@ class Player
     coords = alphabet.index(row), (col.to_i) - 1
   end
 
-  def fire (square, board)
+  def fire(square, board)
+    count = board.positions.length
     board.result(square)
-    change_grid(square)
+    if board.positions.length < count
+      change_to_hit(square)
+    else
+      change_to_x(square)
+    end
   end
 
   def see_grid
     @grid.each { |x| p x }
   end
 
-  def change_grid(square)
+  def change_to_x(square)
     row, col = convertor(square)
     grid[row][col] = "X"
+  end
+
+  def change_to_hit(square)
+    row, col = convertor(square)
+    grid[row][col] = "HIT"
   end
 
 end
